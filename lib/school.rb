d135d00d79ab8f031a35f2b71474eda9349adb75
Damini Varu <damvaru4@gmail.com>
2019-09-26 14:44:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,12 @@
 class School 
   
   
-  attr_accessor :roster, :add_student
+  attr_accessor :name, :roster
   
-  def initialize(roster)
+  def initialize(name, roster)
+    @name = name 
     @roster = []
-    @add_student = add_student
+   
   end 
   
   def add_student
@@ -16,39 +17,13 @@ class School
   
 end 
 
-school = School.new("Bayside High School")
+# school = School.new("Bayside High School")
 
-school.roster
+# school.roster
 
-school.add_student("Zach Morris", 9)
-
-
+# school.add_student("Zach Morris", 9)
 
 
 
-class School
-    attr_accessor :name, :roster
 
-    def initialize (name)
-        @name = name
-        @roster = {}
-    end
 
-    def add_student(student_name, grade_level)
-        roster[grade_level] ||= []
-        roster[grade_level] << student_name
-    end
-
-    def grade(grade_level)
-        roster[grade_level]
-    end
-
-    def sort
-        sorted = {}
-        roster.each do |grade_level, student_name|
-            sorted[grade_level] = student_name.sort
-        end
-        sorted
-    end
-
-end
